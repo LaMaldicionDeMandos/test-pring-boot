93c6d4204ebf945204e3c0475e36e3ec257a51a5
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-02-06 15:56:37 -0300

    simplifico el uso de annotations

--- componentScan/src/main/java/org/pasut/test/spring/boot/componentscan/Main.java
+++ componentScan/src/main/java/org/pasut/test/spring/boot/componentscan/Main.java
@@ -3,14 +3,16 @@ package org.pasut.test.spring.boot.componentscan;
 import org.pasut.test.spring.boot.componentscan.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Todas las annotations comentadas, estan incluidas en la annotation que no está comentada
 @RestController
-@EnableAutoConfiguration
-@ComponentScan
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+@SpringBootApplication
 public class Main {
 	private final Service service;
 	@Autowired
