e4247e3b4ae99fc8e5cfc49d85122b737d77e3ff
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-02-09 16:42:21 -0300

    usando statics

--- web/static/src/main/java/org/pasut/test/spring/boot/web/statics/Main.java
+++ web/static/src/main/java/org/pasut/test/spring/boot/web/statics/Main.java
@@ -1,8 +1,7 @@
-package org.pasut.test.spring.boot.web.simple;
+package org.pasut.test.spring.boot.web.statics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,25 +14,6 @@ public class Main {
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	String hello() {
 		return "Hello";
-	}
-	
-	@RequestMapping(value="/echo/{echo}", method=RequestMethod.GET)
-	String echo(@PathVariable String echo) {
-		return echo;
-	}
-	
-	@RequestMapping(value="/echo/{echo}/{repeat}", method=RequestMethod.GET)
-	String echo(@PathVariable("echo") String value, @PathVariable("repeat") int repeat) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < repeat; i++) {
-			sb.append(" " + value);
-		}
-		return sb.toString();
-	}
-	
-	@RequestMapping(value="/user", method=RequestMethod.GET)
-	User user() {
-		return new User("Marcelo", 34);
 	}
 	
 	public static void main(String[] args) {
